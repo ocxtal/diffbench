629638613c3cab2761ac83eeb5bfacc00b2112ea
Hajime Suzuki <yline5050hs@gmail.com>
2017-04-09 00:24:28 +0900

    fix offset

--- bench.c
+++ bench.c
@@ -519,15 +519,15 @@ int main(int argc, char *argv[])
 	encode(a, alen);
 	encode(b, blen);
 
-	a = add_margin((uint8_t *)a, alen, 16, 16);
-	b = add_margin((uint8_t *)b, blen, 16, 16);
+	a = add_margin((uint8_t *)a, alen, 32, 32);
+	b = add_margin((uint8_t *)b, blen, 32, 32);
 
-	print_result(bench_adaptive_editdist(p, a + 16, alen, b + 16, blen));
-	print_result(bench_edlib(p, a + 16, alen, b + 16, blen));
-	// print_result(bench_ddiag_linear(p, a + 16, alen, b + 16, blen));
-	print_result(bench_ddiag_affine(p, a + 16, alen, b + 16, blen));
-	// print_result(bench_gaba_linear(p, a + 16, alen, b + 16, blen));
-	print_result(bench_gaba_affine(p, a + 16, alen, b + 16, blen));
+	print_result(bench_adaptive_editdist(p, a + 32, alen, b + 32, blen));
+	print_result(bench_edlib(p, a + 32, alen, b + 32, blen));
+	// print_result(bench_ddiag_linear(p, a + 32, alen, b + 32, blen));
+	print_result(bench_ddiag_affine(p, a + 32, alen, b + 32, blen));
+	// print_result(bench_gaba_linear(p, a + 32, alen, b + 32, blen));
+	print_result(bench_gaba_affine(p, a + 32, alen, b + 32, blen));
 
 
 	free(a);
