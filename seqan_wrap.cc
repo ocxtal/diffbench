e12c6ddf0e31828ed5837d9d5fb3623a0956935e
Hajime Suzuki <yline5050hs@gmail.com>
2017-11-20 22:26:51 +0900

    (seqan) add NDEBUG, exclude overheads

--- seqan_wrap.cc
+++ seqan_wrap.cc
@@ -1,4 +1,6 @@
 
+#define NDEBUG		/* disable debugging macros in seqan.h */
+
 #include <string>           // std::string
 #include "seqan/align.h"    // base header of SeqAn::align
 #include <stdint.h>
@@ -7,12 +9,16 @@ using namespace seqan;
 
 extern "C" {
 
+	#include "bench.h"
+	extern bench_t seqan_fill;
 	int seqan_editdist(uint8_t const *a, uint8_t const *b)
 	{
 		typedef String<Dna, CStyle> TSequence;
 		TSequence seq1(a);
 		TSequence seq2(b);
+		bench_start(seqan_fill);
 		int score = globalAlignmentScore(seq1, seq2, MyersBitVector());
+		bench_end(seqan_fill);
 		return(score);
 	}
 };
